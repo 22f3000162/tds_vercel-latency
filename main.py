ae836e9ce6b46d0fc86a360679b2e44fd381724c
22f3000162 <22f3000162@ds.study.iitm.ac.in>
2025-10-17 21:22:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,6 @@ class TelemetryRequest(BaseModel):
     threshold_ms: int
 
 
-@app.get("/")
-def read_root():
-    return {"hello": "vercel"}
 
 @app.post("/")
 def get_metrics(request: TelemetryRequest):
